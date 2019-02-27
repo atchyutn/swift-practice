3025e86653b3b6b9f3552d3795cbe0465fdd3c5c
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2019-02-27 12:00:52 +0530

    title: Understood print function
    desc:
    1. Declared three variables
    2. Used print function
    3. Used interpolation in the print function.

---
+++ print_function.playground/Contents.swift
@@ -0,0 +1,10 @@
+var favoriteWord = "cresent"
+var favoriteColor = "blue"
+var favoriteMusic = "Nirvana"
+
+
+print(favoriteColor)
+print(favoriteWord)
+print(favoriteMusic)
+
+print("Hello World, my favorite color: \(favoriteColor) , my favorite world: \(favoriteWord), my favorite music: \(favoriteMusic)")
